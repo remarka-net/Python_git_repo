53e11570925a97a29da86d8545190115eb5f53f4
canis <pavlouk2012@gmail.com>
2020-02-12 19:04:49 +0200

    initial

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from flask import Flask, escape, request
 
 app = Flask(__name__)
 
-@app.route('/'
+@app.route('/')
 def hello():
-    name = request.args.get("name, World")
-    return f'Hello, {escape(name)}!'
+    name = request.args.get("name", "World")
+    return f'Pavel Bykov TI-71'
 
     if __name__== '__main__':
         app.run('0.0.0.0')
